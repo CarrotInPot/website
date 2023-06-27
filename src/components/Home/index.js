1b66125b96977e88726e6010a159836ab269b7ec
CarrotInPot <vidalfan11@gmail.com>
2023-06-27 16:57:10 +1200

    added animations to the home page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,26 +1,34 @@
 import { useState } from 'react';
 import LogoTitle from '../../assets/images/08-v2-big.png';
-import AnimatedLetters from '../AnimatedLetter';
+import AnimatedLetters from '../AnimatedLetters';
 import './index.scss';
 
 const Home = () => {
-    const [letterClass, setLetterClass] = useState('text-animate')
-    const nameArray = ['s', 'a', 'm']
+    const [letterClass,setLetterClass] = useState('text-animate')
+    const introArray = ['H','i',' ', 't', 'h', 'e', 'r', 'e', '\,']
+    const nameArray = ['I','\'','m',' ','S', 'a', 'm'] 
+    
     return (
         <div className="container home-page">   
      <div className="text-zone">
-        <h1>Hi there, <br /> I'm Sam
+        <h1>        
+        <AnimatedLetters letterClass={letterClass}
+        strArray={introArray}
+        idx={12}/>  
         <br /> 
-        <img src ={LogoTitle} alt ="developer" /> 
         <AnimatedLetters letterClass={letterClass}
         strArray={nameArray}
-        idx={15}/> 
-        <br />  
+        idx={29}/> 
+        <br />
+        <img src ={LogoTitle} alt ="developer" /> 
         </h1>
-        <h2>Software Engineering student at AUT, constantly learning and exploring new technologies in the world of software development and data science.</h2>
+        <h2>Software Engineering student at AUT, 
+            constantly learning and exploring new 
+            technologies in the world of software 
+            development and data science.</h2>
         </div>
     </div> 
     ); 
 }
 
-export default Home     
+export default Home 
