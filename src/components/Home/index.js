c1bb2464680508cf6c015c1bec6195aa1df3a591
CarrotInPot <vidalfan11@gmail.com>
2023-06-28 16:39:01 +1200

    added animations to words and logo

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,12 +1,21 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom'; 
 import LogoTitle from '../../assets/images/08-v2-big.png';
 import AnimatedLetters from '../AnimatedLetters';
 import './index.scss';
+import Logo from './Logo';
 
 const Home = () => {
     const [letterClass,setLetterClass] = useState('text-animate')
-    const introArray = ['H','i',' ', 't', 'h', 'e', 'r', 'e', '\,']
-    const nameArray = ['I','\'','m',' ','S', 'a', 'm'] 
+    const introArray = ['H','i',' ', 't', 'h', 'e', 'r', 'e', ',']
+    const nameArray = ['I','\'','m',' ','S', 'a', 'm', '.']
+    
+    useEffect(() => {
+        const timeoutId = setTimeout(() => {
+            setLetterClass('text-animate-hover')
+        }, 4000);
+        return () => clearTimeout(timeoutId);
+    }, []);
     
     return (
         <div className="container home-page">   
@@ -14,7 +23,7 @@ const Home = () => {
         <h1>        
         <AnimatedLetters letterClass={letterClass}
         strArray={introArray}
-        idx={12}/>  
+        idx={10}/>  
         <br /> 
         <AnimatedLetters letterClass={letterClass}
         strArray={nameArray}
@@ -22,11 +31,15 @@ const Home = () => {
         <br />
         <img src ={LogoTitle} alt ="developer" /> 
         </h1>
-        <h2>Software Engineering student at AUT, 
+        <h2>A Software Engineering student at AUT, 
             constantly learning and exploring new 
             technologies in the world of software 
             development and data science.</h2>
+            <Link to="/contact" className = "flat-button">
+                CONTACT ME 
+            </Link>
         </div>
+        <Logo/>
     </div> 
     ); 
 }
