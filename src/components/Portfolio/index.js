18b1c10ea7f08c59b091b962e7fb65a84e0f9c35
CarrotInPot <vidalfan11@gmail.com>
2023-06-29 18:29:48 +1200

    added images to portfolio

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -2,9 +2,8 @@ import React from "react";
 import "./index.scss"; 
 import Loader from "react-loaders";
 import AnimatedLetters from "../AnimatedLetters";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import portfolioData from '../../data/portfolio.json'; 
-import { useEffect } from "react";
 
 const Portfolio = () => {
     const [letterClass, setLetterClass] = useState('text-animate');
@@ -23,9 +22,19 @@ const Portfolio = () => {
                     portfolio.map((port,idx) => {
                         return(
                             <div className="image-box" key={idx}> 
-                            <img src={port.cover}
+                            <img 
+                            src={require(`${port.cover}`)}
                             className="portfolio-image" 
                             alt = "portfolio" />
+                                <div className="content">
+                                    <p className ="title"> {port.title}</p>
+                                    <h4 className="description">{port.description}</h4>
+                                    <button
+                                    className="btn"
+                                    onClick={()=> window.open(port.url)}>
+                                        View
+                                    </button>
+                                </div>
                             </div>
                         )
                     })
@@ -35,7 +44,6 @@ const Portfolio = () => {
         )
     }
     
-
     return ( 
         <>
          <div className="container portfolio-page">
