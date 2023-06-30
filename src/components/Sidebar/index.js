9db0031e70e8d7ece0b3bce983a51c754cc7fbdf
CarrotInPot <vidalfan11@gmail.com>
2023-06-30 14:08:36 +1200

    added an easter egg :)

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -8,7 +8,7 @@ import { faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => (
     <div className='nav-bar'>
-        <Link className='logo' to='/'>
+        <Link className='logo' to='https://www.youtube.com/watch?v=dQw4w9WgXcQ'>
             <img src={LogoS} alt="logo" />
             <img className="sub-logo" src={LogoSubtitle} alt="samuelmeads" />
         </Link>
